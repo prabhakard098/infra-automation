407a264c381ecc63cccc896d2a3cf69b45edcde2
prabhakard098@gmail.com <prabhakard098@gmail.com>
2022-11-20 01:11:26 +0530

    new update

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rsglabel" {
-    name = "rsgprog3"
+    name = "rsgprog4"
     location = "East US"
     tags = {
       "name" = "practice-rsg"
